589683676659b63bffc520394386474e3f0a2e5d
Psynaps <michaelgunn7@gmail.com>
2024-04-19 20:28:34 -0500

    Implement stack version of dfs

--- Algorithms/DFS.py
+++ Algorithms/DFS.py
@@ -26,4 +26,37 @@ class Solution:
             visit.remove((r, c))
             return count
 
-        return dfs(0, 0, set())
+        # stack based dfs
+        def dfs2(r, c, grid, ROWS, COLS):
+            visited = set()
+            stack = [(r, c)]
+            count = 0
+            while stack:
+                r, c = stack.pop()
+                if (
+                    r not in range(ROWS)
+                    or c not in range(COLS)
+                    or grid[r][c] == 1
+                    or (r, c) in visited
+                ):
+                    continue
+                if r == ROWS - 1 and c == COLS - 1:
+                    count += 1
+                    continue
+                visited.add((r, c))
+                stack.append((r + 1, c))
+                stack.append((r - 1, c))
+                stack.append((r, c + 1))
+                stack.append((r, c - 1))
+            return count
+
+        # return dfs(0, 0, set())
+        return dfs2(0, 0, grid, ROWS, COLS)
+
+
+# Test Cases
+solution = Solution()
+# Example 1
+grid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+# Expected: 2
+print(solution.countPaths(grid))
